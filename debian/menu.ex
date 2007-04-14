dc8bdbee26194f19772ce9b669b2b4a9dbd2372d
Bernard Gray <bernard.gray@gmail.com>
2007-04-14 05:42:08 +0000

    [svn-inject] Installing original source of sidux-art-gfxbootsplash

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(sidux-art-gfxbootsplash):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="sidux-art-gfxbootsplash" command="/usr/bin/sidux-art-gfxbootsplash"
---
+++ debian/sidux-art-gfxbootsplash-default.ex
@@ -0,0 +1,10 @@
+# Defaults for sidux-art-gfxbootsplash initscript
+# sourced by /etc/init.d/sidux-art-gfxbootsplash
+# installed at /etc/default/sidux-art-gfxbootsplash by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
